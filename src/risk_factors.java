f472572ab2ce70ba4e861d3090ce53edbb0b97c6
Rahul Shukla <rahulshukla3012@gmail.com>
2017-03-02 19:54:05 +0530

    Update risk_factors.java

--- src/risk_factors.java
+++ src/risk_factors.java
@@ -4,17 +4,7 @@ import java.awt.event.*;
 import java.net.*;
 import java.io.*;
 
-/**
- This program has been solely programmed for an AI project and not meant for any business means.
- If you intend to you the code please do so freely on condition you acknowledge us.
- Created by Group 13 - CyberVision softwares.
- The ODBC datasource name is AI please do not forget to set that before running then program.
- Please do not hesitate to submit any bug to us.
- you can mail us on dh002211@ex.apiit.edu.my
 
- @author Asrar Ahmed Makrani
- @version 1.4.0 build 3
- */
 
 public class risk_factors extends JPanel
 {
